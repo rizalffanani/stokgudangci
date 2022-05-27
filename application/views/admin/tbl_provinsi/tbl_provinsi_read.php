f2120b4a70b312b714375a1ace0ef0d1b4f0e376
Rizal <rizalffanani@gmail.com>
2022-05-27 13:00:13 +0700

    dlt

--- application/views/admin/tbl_provinsi/tbl_provinsi_read.php
+++
@@ -1,9 +0,0 @@
-<table class="table">
-	    <tr><td>Nama Prov</td><td><?php echo $nama_prov; ?></td></tr>
-	    <tr><td>Satuan</td><td><?php echo $satuan; ?></td></tr>
-	    <tr><td>Eselon1</td><td><?php echo $eselon1; ?></td></tr>
-	    <tr><td>Eselon2</td><td><?php echo $eselon2; ?></td></tr>
-	    <tr><td>Eselon3</td><td><?php echo $golongan4; ?></td></tr>
-	    <tr><td>Eselon4</td><td><?php echo $golongan321; ?></td></tr>
-	    <tr><td></td><td><a href="<?php echo site_url('admin/tbl_provinsi') ?>" class="btn btn-default">Cancel</a></td></tr>
-	</table>
--- application/views/admin/tbl_tamu/tbl_tamu_read.php
+++
@@ -1,5 +0,0 @@
-<table class="table">
-	    <tr><td>Nama Tamu</td><td><?php echo $nama_tamu; ?></td></tr>
-	    <tr><td>Daftar Tamu</td><td><?php echo $daftar_tamu; ?></td></tr>
-	    <tr><td></td><td><a href="<?php echo site_url('admin/tbl_tamu') ?>" class="btn btn-default">Cancel</a></td></tr>
-	</table>
